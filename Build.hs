ceee04002a45bdb587a9fda93282162389305620
Robert Goss <goss.robert@gmail.com>
2015-03-09 16:30:53 +0000

    Added undefined stubs. Removed primariness as a build property

--- Build.hs
+++ Build.hs
@@ -4,9 +4,8 @@ import Package
 
 import Data.Map.Lazy as Map
 
-data BuildData = BuildData { isPrimary :: Bool,
-							 package :: Package,
-							 dependencies :: [BuildData]
+data BuildData = BuildData { 
+                             dependencies :: [BuildData]
 						   } deriving(Eq,Ord)
 
 data BuildResult = BuildSuccess       -- Build complete success.
@@ -23,29 +22,35 @@ data BuildDatabase = BuildDatabase (Map.Map BuildData BuildResult) -- Map of a b
 --Basic constructors of the full build database - either from a file or directly computed from system.
 --  These are the only constructors to be exported from this module so no partially constructed databases are made.
 fromPackageDatabase :: PackageDatabase -> IO BuildDatabase
+fromPackageDatabase = undefined
 
 --Sub contructors used internally in this module to construct the package database from the system
 
 --Add the following build to the build database
 addBuild :: Build -> BuildDatabase -> BuildDatabase
+addBuild = undefined
 
 --Build the following package on the system
 build :: BuildDatabase -> BuildData -> IO BuildResult
+build = undefined
 
 --Get the primary build data associated to the pacakge with the given name in the package database.
 getBuildData :: PackageName -> PackageDatabase -> BuildData
+getBuildData = undefined
 
 
 --Properties of the BuildDatabase that can be extracted
 
 --Get all the builds in the build database
 getAllBuilds :: BuildDatabase -> [Build]
+getAllBuilds = undefined
 
 --Get all the builds in the build database that are the primary builds of a package
 -- And thus have no restriction on their dependencies
 getPrimaryBuilds :: BuildDatabase -> [Build]
+getPrimaryBuilds = undefined
 
 --Get the build in the build database that is the primary build associatede to the package in the package 
 -- database with the given name.
 getPrimaryBuild :: BuildDatabase -> PackageName -> Build
-
+getPrimaryBuild = undefined
