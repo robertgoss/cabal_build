f5932aacb50e6e00373fc1dd19d82ce72dbbdc69
Robert Goss <goss.robert@gmail.com>
2015-03-09 16:58:17 +0000

    Added impl for build

--- Build.hs
+++ Build.hs
@@ -1,18 +1,20 @@
 module Build() where
 
 import Package
+import qualified System
 
-import Data.Map as Map
+import qualified Data.Map as Map
 
 data BuildData = BuildData { 
                              package :: PackageName,
                              dependencies :: [BuildData]
-						   } deriving(Eq,Ord)
+                           } deriving(Eq,Ord)
 
 data BuildResult = BuildSuccess       -- Build complete success.
                  | ResolutionFailure  -- Failed to resolve the dependencies of this package.
                  | DependentFailure   -- A package build that this package depends on failed to build.
                  | BuildFail          -- Build failed in progress of building this package.
+                 deriving(Eq,Show,Ord)
 
 data Build = Build BuildData BuildResult
             
@@ -33,12 +35,30 @@ emptyBuildDatabase = BuildDatabase Map.empty Map.empty
 
 --Add the following build to the build database
 addBuild :: Build -> BuildDatabase -> BuildDatabase
-addBuild = (Build buildData buildResult) (BuildDatabase resultMap primaryMap) = BuildDatabase resultMap' primaryMap
-	where resultMap' = Map.insert buildData buildResult resultMap
+addBuild (Build buildData buildResult) (BuildDatabase resultMap primaryMap) = BuildDatabase resultMap' primaryMap
+    where resultMap' = Map.insert buildData buildResult resultMap
 
 --Build the following package on the system
+-- If the results are already in the build database it will return that.
+--First lookup to see if the resut is in the database if so return it.
 build :: BuildDatabase -> BuildData -> IO BuildResult
-build = undefined
+build db@(BuildDatabase resultMap _) buildData = case Map.lookup buildData resultMap of
+                                                    (Just result) -> return result
+                                                      --If package is not in database find the build type for all dependencies
+                                                    Nothing -> do dependentResults <- mapM (build db) dependentBuilds
+                                                                  if all success dependentResults then
+                                                                    --If all dependencies succeed then build package on the system.
+                                                                    fmap resultType $ System.build buildPackageList
+                                                                  else
+                                                                    --If any fail then return a dependence failure
+                                                                    return DependentFailure
+        where dependentBuilds = dependencies buildData :: [BuildData]
+              --If a result is classed as successful and a translation of the system build into a result type
+              success = (== BuildSuccess)
+              resultType True = BuildSuccess
+              resultType False = BuildFail
+              --The package names of all the packages to use in this build
+              buildPackageList = package buildData : map package dependentBuilds
 
 --Get the primary build data associated to the pacakge with the given name in the package database.
 getBuildData :: PackageName -> PackageDatabase -> BuildData
