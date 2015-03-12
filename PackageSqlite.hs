bdcfd68a9e990d67b02a4ff129e595bda5725259
Robert Goss <goss.robert@gmail.com>
2015-03-12 23:19:54 +0000

    Fix database to avoid depencencies being added to a column as string. Use better practice of normalizing relations

--- PackageSqlite.hs
+++ PackageSqlite.hs
@@ -14,23 +14,31 @@ import Database.Persist
 import Database.Persist.TH
 import Database.Persist.Sqlite
 import Data.Conduit
-import Data.Conduit.List
+import qualified Data.Conduit.List as CL
+import Data.Maybe(fromJust)
 
 import qualified Package 
 
 type VersionString = String
 
+--Setup a datatype to store packages as persist objects
+
 share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
 PackageSqlite
-    name Package.PackageName --Store the full package name
-    dependencies [Package.PackageName] Maybe --PackageDependencies but mkPersist need alternative def.
-    UniqueName name -- The name should be unique.
+    name Package.PackageName -- Allow pulling name list out of database without reconstructing dependencies
+    resolutionFailed Bool -- If the resolution failed
+    UniqueName name
+    deriving Show 
+PackageDependence
+    package PackageSqliteId
+    dependant Package.PackageName
     deriving Show
 |]
 
 
 --We do not expose this constructor so the only availible constructor creates an empty database
-data PackageDatabaseSqlite = PackageDatabaseSqlite
+--Keep a set of the current packages
+data PackageDatabaseSqlite = PackageDatabaseSqlite 
 
 --Mke sure migrations are run in load
 loadDatabase :: IO PackageDatabaseSqlite
@@ -42,21 +50,34 @@ loadDatabase = runSqlite "package-sqlite.data" $ do runMigration migrateAll
 instance Package.PackageDatabase PackageDatabaseSqlite where
     emptyDatabase = runSqlite "package-sqlite.data" $ do runMigration migrateAll
                                                          return PackageDatabaseSqlite
+
     keys _ = runSqlite "package-sqlite.data" $ keysQuery
+
     insert name deps _ = runSqlite "package-sqlite.data" $ do insertQuery name deps
                                                               return PackageDatabaseSqlite
     getDependency _ name = runSqlite "package-sqlite.data" $ dependenceQuery name
 
---Required queries
+
 --Get database keys - no filter only return name
--- Use an conduit and filte the list as go so dont need all in memory
-keysQuery =  selectSource [] [] $$ Data.Conduit.List.map onlyName =$ consume
-    where onlyName = packageSqliteName . entityVal
+-- Use an conduit and filte the list as go so dont need all dpendencies in memory when 
+keysQuery = selectSource [] [] $= CL.map onlyName $$ CL.consume
+        where onlyName = packageSqliteName . entityVal
+--Required queries
 
-insertQuery name deps = insert $ PackageSqlite name deps
+insertQuery name deps
+   | deps == Nothing = insert_ $ PackageSqlite name True -- The resolution has failed do not add more packages
+   | otherwise = do pkgId <- insert $ PackageSqlite name False
+                    insertMany_ $ zipWith PackageDependence (repeat pkgId) (fromJust deps)
 
-dependenceQuery name = do package <- getBy $ UniqueName name
-                          return . fmap (packageSqliteDependencies . entityVal) $ package
+dependenceQuery name = do package' <- getBy $ UniqueName name
+                          case package' of 
+                            Nothing -> return Nothing -- Could not find the package returning nothing
+                            Just package -> if resolutionFailed package then return $ Just Nothing -- Package found but resolution failed
+                                            else fmap (Just . Just) $ getDependenceQuery $ entityKey package --Perform the query to get dependencies
+
+    where getDependenceQuery packageId = do dependencies <- selectList [PackageDependencePackage ==. packageId] []
+                                            return . Prelude.map (packageDependenceDependant . entityVal) $ dependencies
+          resolutionFailed = packageSqliteResolutionFailed . entityVal
 
 
 
