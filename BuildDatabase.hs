8ee0c79e9eed0e19639c67b9de46192dbd623685
Robert Goss <goss.robert@gmail.com>
2015-04-01 23:26:56 +0100

    Added postgres to build database and changed filename. Added cache in database for number of dependencies

--- BuildDatabase.hs
+++ BuildDatabase.hs
@@ -8,17 +8,24 @@
 {-# LANGUAGE TemplateHaskell            #-}
 {-# LANGUAGE TypeFamilies               #-}
 
-module BuildSqlite(BuildDatabaseSqlite,
-                   loadBuildDatabase) where
+module BuildDatabase(BuildDatabaseSqlite,
+                     loadSqliteBuildDatabase,
+                     BuildDatabasePostgres,
+                     loadPostgresBuildDatabase,) where
 
 import Database.Persist
 import Database.Persist.TH
 import Database.Persist.Sqlite
+import Database.Persist.Postgresql
 import Data.Conduit
 import qualified Data.Conduit.List as CL
 import Data.Maybe(fromJust, isJust)
-
 import Debug.Trace(trace)
+
+import Control.Monad.Trans.Resource(runResourceT,ResourceT)
+import Control.Monad.Logger(runNoLoggingT,NoLoggingT)
+import Control.Monad.IO.Class (MonadIO (..))
+import Control.Monad.Trans.Control (MonadBaseControl)
 
 import qualified Build
 import qualified Package
@@ -27,6 +34,15 @@ import qualified BuildResult
 import qualified Data.Set as Set
 
 import qualified Data.Text as T
+
+--Indexes 
+
+--persistent cannot create objects for speed should have them on:
+--  BuildData - buildHash
+--  Result - buildId
+--  Primary package
+--  BuildDependance build
+--  PackageDependance build
 
 --Setup a datatype to store build data as persist objects
 -- Also store the results map, id map and primary map
@@ -58,35 +74,82 @@ PackageDependance -- Expresses relation of a build being dependant on a package.
     deriving Show
 |] 
 
+
 --Database class - as we do not export the construtor the database can only be accessed through load and empty
 -- And can only be modified though build database after this.
+--Sqlite database
 data BuildDatabaseSqlite = BuildDatabaseSqlite
 
 --Make sure migrations are run in load
-loadBuildDatabase :: IO BuildDatabaseSqlite
-loadBuildDatabase = do runSqlite "build-sqlite.data" $ runMigration migrateAll
-                       return BuildDatabaseSqlite
+loadSqliteBuildDatabase :: IO BuildDatabaseSqlite
+loadSqliteBuildDatabase = do runSqlite "build-sqlite.data" $ runMigration migrateAll
+                             return BuildDatabaseSqlite
 
+--Hardcoded sqlite file
+sqliteFile = "build-sqlite.data"
 
 instance Build.BuildDatabase BuildDatabaseSqlite where
-    emptyBuildDatabase = runSqlite "build-sqlite.data" $ do runMigration migrateAll
-                                                            return $ BuildDatabaseSqlite 
+    emptyBuildDatabase = runSqlite sqliteFile $ do runMigration migrateAll
+                                                   return $ BuildDatabaseSqlite 
 
-    addId buildId buildData _ = runSqlite "build-sqlite.data" $ do addIdQuery buildId buildData
-                                                                   return BuildDatabaseSqlite
+    addId buildId buildData _ = runSqlite sqliteFile $ do addIdQuery buildId buildData
+                                                          return BuildDatabaseSqlite
 
-    addResult buildId buildResult db = runSqlite "build-sqlite.data" $ do addResultQuery buildId buildResult
-                                                                          return db
-    addPrimary package buildId db = runSqlite "build-sqlite.data" $ do addPrimaryQuery package buildId
-                                                                       return db
+    addResult buildId buildResult db = runSqlite sqliteFile $ do addResultQuery buildId buildResult
+                                                                 return db
+    addPrimary package buildId db = runSqlite sqliteFile $ do addPrimaryQuery package buildId
+                                                              return db
 
-    getData _ buildId = runSqlite "build-sqlite.data" $ getDataQuery buildId
+    getData _ buildId = runSqlite sqliteFile $ getDataQuery buildId
 
-    getResult _ buildId = runSqlite "build-sqlite.data" $ getResultQuery buildId
+    getResult _ buildId = runSqlite sqliteFile $ getResultQuery buildId
 
-    allIds _ = runSqlite "build-sqlite.data" $ allIdsQuery
+    allIds _ = runSqlite sqliteFile $ allIdsQuery
 
-    idExists _ buildId = runSqlite "build-sqlite.data" $ idExistsQuery buildId
+    idExists _ buildId = runSqlite sqliteFile $ idExistsQuery buildId
+
+--Database class - as we do not export the construtor the database can only be accessed through load and empty
+-- And can only be modified though build database after this.
+--Postgres database
+data BuildDatabasePostgres = BuildDatabasePostgres
+
+--Make sure migrations are run in load
+loadPostgresBuildDatabase :: IO BuildDatabasePostgres
+loadPostgresBuildDatabase = do runSqlite "build-sqlite.data" $ runMigration migrateAll
+                               return BuildDatabasePostgres
+
+--Hardcoded connection string
+connectionString = "host=localhost user=cabal_build password=cabal dbname=cabal_build"
+
+--Helper function to run a postgres connection throws away loging
+-- Does nt preserve connection between queries
+runPostgres :: (MonadBaseControl IO m, MonadIO m)
+          => SqlPersistT (NoLoggingT (ResourceT m)) a -- ^ database action
+          -> m a
+runPostgres = runResourceT
+            . runNoLoggingT
+            . withPostgresqlConn connectionString
+            . runSqlConn
+
+instance Build.BuildDatabase BuildDatabasePostgres where
+    emptyBuildDatabase = runPostgres $ do runMigration migrateAll
+                                          return BuildDatabasePostgres
+
+    addId buildId buildData _ = runPostgres $ do addIdQuery buildId buildData
+                                                 return BuildDatabasePostgres
+
+    addResult buildId buildResult db = runPostgres $ do addResultQuery buildId buildResult
+                                                        return db
+    addPrimary package buildId db = runPostgres $ do addPrimaryQuery package buildId
+                                                     return db
+
+    getData _ buildId = runPostgres $ getDataQuery buildId
+
+    getResult _ buildId = runPostgres $ getResultQuery buildId
+
+    allIds _ = runPostgres allIdsQuery
+
+    idExists _ buildId = runPostgres $ idExistsQuery buildId
 
 --Required Queries
 --Get database ids - no filter only return name
@@ -132,10 +195,13 @@ addIdQuery buildId buildData = do buildSqliteId <- insertUnique buildDataSqlite
           buildDependencies' = Build.buildDependencies buildData
           packageDependencies' = Build.packageDependencies buildData
           resFailure = buildDependencies' == Nothing -- A resolution has failed if buildDependencies contains nothing
-          buildDataSqlite = BuildData package' resFailure buildId -- The buildData
+          buildDataSqlite = BuildData package' resFailure buildId nDeps-- The buildData
           --From just dep and pdeps -- only use after resolution shown to exist
           deps = fromJust buildDependencies'
           pDeps = fromJust packageDependencies'
+          nDeps = case (buildDependencies') of  --Get the number of dependencies set to 0 is there is a resolution failure.
+                     Nothing -> 0
+                     (Just ds) -> length ds
 
 --Add result by finding result with build id and updating it's build result
 addResultQuery buildId buildResult = do entity <- fmap fromJust $ getBy $ UniqueResultId buildId
